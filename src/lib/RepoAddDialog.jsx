f7a3d60694989db11953ca6e5b57e403e19592af
kolotev <kolotev@gmail.com>
2019-04-25 12:38:08 -0400

    Fixed errors, rafactored chane of callbacks, movmed reset* functions

--- src/lib/RepoAddDialog.jsx
+++ src/lib/RepoAddDialog.jsx
@@ -20,305 +20,305 @@ import DependantAsyncSelect from "./DependantAsyncSelect";
 import axios from "axios";
 
 const styles = theme => ({
-    root: {
-        overflow: 'visible'
-    },
-    select: {
-        margin: `${theme.spacing(1)}px 0`
-    }
+  root: {
+    overflow: "visible"
+  },
+  select: {
+    margin: `${theme.spacing(1)}px 0`
+  }
 });
 
 class RepoAddDialog extends React.Component {
-    state = {
-        selectProjects: {
-            loaded: false,
-            options: [],
-            selectedOption: null
+  state = {
+    selectProjects: {
+      loaded: false,
+      options: [],
+      selectedOption: null
+    },
+    selectRepos: {
+      loaded: false,
+      options: [],
+      selectedOption: null
+    },
+    gitUrl: ""
+  };
+
+  iState = this.state; // initial state
+  iStateSelectProjects = this.iState.selectProjects;
+  iStateSelectProjects = this.iState.selectRepos;
+  iStategitUrl = this.iState.gitUrl;
+  reposSelect = null; // :React.ElementRef<AsyncSelect>;
+
+  resetSelectProjects = callback => {
+    this.resetSelectRepos(() =>
+      this.setState(
+        {
+          selectProjects: this.iStateSelectProjects
         },
-        selectRepos: {
-            loaded: false,
-            options: [],
-            selectedOption: null
+        () => callback()
+      )
+    );
+  };
+
+  resetSelectRepos = callback => {
+    this.resetGitUrl(() =>
+      this.setState(
+        {
+          selectRepos: this.iStateSelectProjects
         },
-        gitUrl: ""
-    };
+        () => callback()
+      )
+    );
+  };
 
-    iState = this.state; // initial state
-    iStateSelectProjects = this.iState.selectProjects;
-    iStateSelectProjects = this.iState.selectRepos;
-    iStategitUrl = this.iState.gitUrl;
-    reposSelect = null; // :React.ElementRef<AsyncSelect>;
+  resetGitUrl = callback => {
+    this.setState(
+      {
+        GitUrl: this.iStateGitUrl
+      },
+      () => callback()
+    );
+  };
 
-    filterProjects = inputValue => {
-        return this.state.selectProjects.options.filter(p =>
-            p.label.toLowerCase().includes(inputValue.toLowerCase())
-        );
-    };
+  filterProjects = inputValue => {
+    return this.state.selectProjects.options.filter(p =>
+      p.label.toLowerCase().includes(inputValue.toLowerCase())
+    );
+  };
 
-    filterRepos = inputValue => {
-        return this.state.selectRepos.options.filter(r =>
-            r.label.toLowerCase().includes(inputValue.toLowerCase())
-        );
-    };
+  filterRepos = inputValue => {
+    return this.state.selectRepos.options.filter(r =>
+      r.label.toLowerCase().includes(inputValue.toLowerCase())
+    );
+  };
 
-    loadOptionsProjects = inputValue =>
-        new Promise(resolve => {
-            const url =
-                "https://test.ncbi.nlm.nih.gov/ipmc-dev11/ka/bb_projects.cgi";
-            if (this.state.selectProjects.loaded)
-                resolve(this.filterProjects(inputValue));
-            else {
-                console.info(`loadOptionsProjects() url: ${url}`);
-                axios
-                    .get(url)
-                    .then(response => {
-                        let projects = response.data.map(
-                            ({ id, name, key }, idx) => ({
-                                label: name,
-                                value: key,
-                                id: id,
-                                idx: idx
-                            })
-                        );
-                        this.setState({
-                            selectProjects: {
-                                loaded: true,
-                                selectedOption: null,
-                                options: projects
-                            }
-                        });
-                        resolve(this.filterProjects(inputValue));
-                    })
-                    .catch(error => console.error(error));
-            }
-        });
-
-    loadOptionsRepos = inputValue =>
-        new Promise(resolve => {
-            const { selectProjects, selectRepos } = this.state;
-            if (selectProjects.loaded === null) resolve([]);
-            else {
-                const url = `https://test.ncbi.nlm.nih.gov/ipmc-dev11/ka/bb_repos.cgi?project=${
-                    selectProjects.selectedOption.value
-                }`;
-
-                if (selectRepos.loaded) resolve(this.filterRepos(inputValue));
-                else {
-                    console.info(`loadOptionsRepos() url: ${url}`);
-                    axios
-                        .get(url)
-                        .then(response => {
-                            let repos = response.data.map(
-                                ({ id, name, slug, links }, idx) => ({
-                                    label: name,
-                                    value: slug,
-                                    id: id,
-                                    idx: idx,
-                                    git_url: links.clone[1]
-                                })
-                            );
-                            this.setState({
-                                selectRepos: {
-                                    loaded: true,
-                                    selectedOption: null,
-                                    options: repos
-                                }
-                            });
-                            resolve(this.filterRepos(inputValue));
-                        })
-                        .catch(error => console.error(error));
+  loadOptionsProjects = inputValue =>
+    new Promise(resolve => {
+      const url = "https://test.ncbi.nlm.nih.gov/ipmc-dev11/ka/bb_projects.cgi";
+      if (this.state.selectProjects.loaded)
+        resolve(this.filterProjects(inputValue));
+      else {
+        console.info(`loadOptionsProjects() url: ${url}`);
+        axios
+          .get(url)
+          .then(response => {
+            let projects = response.data.map(({ id, name, key }, idx) => ({
+              label: name,
+              value: key,
+              id: id,
+              idx: idx
+            }));
+            this.setState(
+              {
+                selectProjects: {
+                  loaded: true,
+                  selectedOption: null,
+                  options: projects
                 }
-            }
-        });
+              },
+              () => resolve(this.filterProjects(inputValue))
+            );
+          })
+          .catch(error => console.error(error));
+      }
+    });
 
-    resetSelectProjects = () => {
-        this.setState({
-            selectProjects: this.iStateSelectProjects
-        });
-        this.resetSelectRepos();
-    };
+  loadOptionsRepos = inputValue =>
+    new Promise(resolve => {
+      const { selectProjects, selectRepos } = this.state;
+      if (selectProjects.loaded === null) resolve([]);
+      else {
+        const url = `https://test.ncbi.nlm.nih.gov/ipmc-dev11/ka/bb_repos.cgi?project=${
+          selectProjects.selectedOption.value
+        }`;
 
-    resetSelectRepos = () => {
-        this.setState({
-            selectRepos: this.iStateSelectProjects
-        });
-        this.resetGitUrl();
-    };
-
-    resetGitUrl = () => {
-        this.setState({
-            GitUrl: this.iStateGitUrl
-        });
-    };
-
-    onChangeProjects = (selectOption, { action }) => {
-        switch (action) {
-            case "select-option":
-                this.setState(
-                    {
-                        selectProjects: {
-                            selectedOption: selectOption
-                        },
-                        selectRepos: {
-                            selectedOption: null
-                        },
-                        gitUrl: ""
-                    },
-                    () => this.reposSelect.focus()
-                );
-                return;
-            default:
-                this.resetSelectProjects();
-                return;
-        }
-    };
-
-    onChangeRepos = (selectedOption, { action }) => {
-        console.info(
-            `onChangeRepos() selectedOption.value: ${selectedOption}, action: ${action}`
-        );
-        switch (action) {
-            case "select-option":
-                this.setState({
-                    selectRepos: {
-                        selectedOption: selectedOption
-                    },
-                    gitUrl: selectedOption.git_url.href
-                });
-
-                return;
-            default:
-                this.resetSelectRepos();
-                return;
-        }
-    };
-
-    onExit = e => {
-        this.setState(this.iState);
-        if (typeof this.props.onExit === "function") this.props.onExit(e);
-    };
-
-    render() {
-        const { state, props } = this;
-        const { classes, ...otherProps } = props;
-        const selectStyles = {
-            menu: base => ({
-                ...base,
-                zIndex: 100
+        if (selectRepos.loaded) resolve(this.filterRepos(inputValue));
+        else {
+          console.info(`loadOptionsRepos() url: ${url}`);
+          axios
+            .get(url)
+            .then(response => {
+              let repos = response.data.map(
+                ({ id, name, slug, links }, idx) => ({
+                  label: name,
+                  value: slug,
+                  id: id,
+                  idx: idx,
+                  git_url: links.clone[1]
+                })
+              );
+              this.setState(
+                {
+                  selectRepos: {
+                    loaded: true,
+                    selectedOption: null,
+                    options: repos
+                  }
+                },
+                () => {
+                  resolve(this.filterRepos(inputValue));
+                  this.reposSelect.focus();
+                }
+              );
             })
-        };
+            .catch(error => console.error(error));
+        }
+      }
+    });
 
-
-<DialogContent className={classes.root}></DialogContent>
-
-        return (
-            <Dialog
-                classes={{ paperScrollPaper: classes.root }}
-                {...otherProps}
-                onExit={this.onExit}
-                aria-labelledby="form-dialog-title"
-            >
-                <form
-                    action="/"
-                    method="POST"
-                    onSubmit={e => {
-                        e.preventDefault();
-                        alert("Submitted form!");
-                        this.handleClose();
-                    }}
-                >
-                    <DialogTitle id="form-dialog-title">
-                        Add a Repository
-                    </DialogTitle>
-                    
-                    <DialogContent className={classes.root}>
-                        <DialogContentText>
-                            To add an NCBI BitBucket Repository please choose
-                            the project and repo from the lists below.
-                        </DialogContentText>
-                        <AsyncSelect
-                            key="project"
-                            id="bb-project-select"
-                            styles={selectStyles}
-                            className={classes.select}
-                            name="bb-project"
-                            placeholder="Select Project*"
-                            openMenuOnFocus
-                            autoFocus
-                            cacheOptions
-                            defaultOptions
-                            onChange={this.onChangeProjects}
-                            loadOptions={this.loadOptionsProjects}
-                            value={state.selectProjects.selectedOption}
-                            isSearchable
-                            menuPlacement="auto"
-                        />
-                        <DependantAsyncSelect
-                            key="repo"
-                            ref={ref => {
-                                this.reposSelect = ref;
-                            }}
-                            id="bb-repo-select"
-                            styles={selectStyles}
-                            className={classes.select}
-                            placeholder="Select Repository*"
-                            name="bb-repo"
-                            openMenuOnFocus
-                            defaultOptions
-                            isDisabled={
-                                state.selectProjects.selectedOption === null
-                            }
-                            onChange={this.onChangeRepos}
-                            loadOptions={this.loadOptionsRepos}
-                            dependantLoadOptionsArgs={
-                                state.selectProjects.selectedOption !== null
-                                    ? state.selectProjects.selectedOption.value
-                                    : null
-                            }
-                            value={state.selectRepos.selectedOption}
-                            isSearchable
-                            menuPlacement="auto"
-                        />
-                        <TextField
-                            key="git-url"
-                            id="git-url-tf"
-                            label="Git URL"
-                            /*helperText="Another Gut URL"*/
-                            value={state.gitUrl}
-                            margin="normal"
-                            fullWidth
-                            InputProps={{
-                                readOnly: true
-                            }}
-                            InputLabelProps={{
-                                shrink: true
-                            }}
-                            variant="filled"
-                        />
-                    </DialogContent>
-                    <DialogActions>
-                        <Button
-                            aria-label="Cancel"
-                            onClick={this.props.onClose}
-                            color="default"
-                            variant="outlined"
-                        >
-                            Cancel
-                        </Button>
-                        <Button
-                            aria-label="Save"
-                            variant="contained"
-                            color="primary"
-                            type="submit" //set the buttom type is submit
-                            form="save_repo_form" //target the form which you want to sent
-                        >
-                            Save
-                        </Button>
-                    </DialogActions>
-                </form>
-            </Dialog>
+  onChangeProjects = (selectOption, { action }) => {
+    const { selectProjects } = this.state;
+    switch (action) {
+      case "select-option":
+        this.resetSelectRepos(() =>
+          this.setState({
+            selectProjects: {
+              loaded: selectProjects.loaded,
+              selectedOption: selectOption,
+              options: selectProjects.options
+            }
+          })
         );
+        return;
+      default:
+        this.resetSelectProjects();
+        return;
     }
+  };
+
+  onChangeRepos = (selectedOption, { action }) => {
+    const { selectRepos } = this.state;
+    switch (action) {
+      case "select-option":
+        this.resetGitUrl(() =>
+          this.setState({
+            selectRepos: {
+              loaded: selectRepos.loaded,
+              selectedOption: selectedOption,
+              options: selectRepos.options
+            },
+            gitUrl: selectedOption.git_url.href
+          })
+        );
+        return;
+      default:
+        this.resetSelectRepos();
+        return;
+    }
+  };
+
+  onExit = e => {
+    this.setState(this.iState);
+    if (typeof this.props.onExit === "function") this.props.onExit(e);
+  };
+
+  render() {
+    const { state, props } = this;
+    const { classes, ...otherProps } = props;
+    const selectStyles = {
+      menu: base => ({
+        ...base,
+        zIndex: 100
+      })
+    };
+
+    <DialogContent className={classes.root} />;
+
+    return (
+      <Dialog
+        classes={{ paperScrollPaper: classes.root }}
+        {...otherProps}
+        onExit={this.onExit}
+        aria-labelledby="form-dialog-title"
+      >
+        <form
+          action="/"
+          method="POST"
+          onSubmit={e => {
+            e.preventDefault();
+            alert("Submitted form!");
+            this.handleClose();
+          }}
+        >
+          <DialogTitle id="form-dialog-title">Add a Repository</DialogTitle>
+
+          <DialogContent className={classes.root}>
+            <DialogContentText>
+              To add an NCBI BitBucket Repository please choose the project and
+              repo from the lists below.
+            </DialogContentText>
+            <AsyncSelect
+              id="bb-project-select"
+              styles={selectStyles}
+              className={classes.select}
+              name="bb-project"
+              placeholder="Select Project*"
+              onChange={this.onChangeProjects}
+              loadOptions={this.loadOptionsProjects}
+              defaultOptions
+              cacheOptions
+              value={state.selectProjects.selectedOption}
+              openMenuOnFocus
+              autoFocus
+            />
+            <DependantAsyncSelect
+              id="bb-repo-select"
+              ref={ref => {
+                this.reposSelect = ref;
+              }}
+              styles={selectStyles}
+              className={classes.select}
+              placeholder="Select Repository*"
+              name="bb-repo"
+              isDisabled={!state.selectRepos.loaded}
+              onChange={this.onChangeRepos}
+              loadOptions={this.loadOptionsRepos}
+              defaultOptions
+              cacheOptions
+              dependantLoadOptionsArgs={state.selectProjects.selectedOption}
+              value={state.selectRepos.selectedOption}
+              openMenuOnFocus
+            />
+            <TextField
+              key="git-url"
+              id="git-url-tf"
+              label="Git URL"
+              /*helperText="Another Gut URL"*/
+              value={state.gitUrl}
+              margin="normal"
+              fullWidth
+              InputProps={{
+                readOnly: true
+              }}
+              InputLabelProps={{
+                shrink: true
+              }}
+              variant="filled"
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button
+              aria-label="Cancel"
+              onClick={this.props.onClose}
+              color="default"
+              variant="outlined"
+            >
+              Cancel
+            </Button>
+            <Button
+              aria-label="Save"
+              variant="contained"
+              color="primary"
+              type="submit" //set the buttom type is submit
+              form="save_repo_form" //target the form which you want to sent
+            >
+              Save
+            </Button>
+          </DialogActions>
+        </form>
+      </Dialog>
+    );
+  }
 }
 
 const StyledRepoAddDialog = withStyles(styles)(RepoAddDialog);
